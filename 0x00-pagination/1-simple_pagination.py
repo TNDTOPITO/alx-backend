4e2825c5a94371ba6bbc217038d383dd14dcf384
Thabiso <940303tabiso@gamil.com>
2023-04-15 08:03:29 +0000

    Add Task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,14 +7,6 @@ from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-    if page and page_size:
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return start_index, end_index
-
-
-class Server:
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
@@ -34,12 +26,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ obtains the indexes and return corresponding pages """
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
-        start, end = index_range(page, page_size)
-        pages = []
-        if start >= len(self.dataset()):
-            return pages
-        pages = self.dataset()
-        return pages[start:end]
+        pass
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """ returns tuple of size two containing start index, end index """
+        pass
