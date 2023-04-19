2511b598c4391ddbdf2396cfdf39efe5327ae80e
Thabiso <940303tabiso@gamil.com>
2023-04-19 12:02:18 +0000

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python3
-""" Python caching systems """
-
+#!/usr/bin/python3
+"""  Basic dictionary """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ class BasicCache that inherits from BaseCaching and is a caching system """
-
+    """ Class that inherits from BaseCaching and is a caching system
+        This caching system doesn’t have limit """
     def put(self, key, item):
-        """ Add an item in the cache """
+        """ Assign to the dictionary """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get the value linked """
-        if key is None or self.catch_data.get(key) is None:
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
         return self.cache_data.get(key)
