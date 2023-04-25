702fcfd8020b41bd4f58722b2ef683a6a44ac695
Thabiso <940303tabiso@gamil.com>
2023-04-25 13:17:04 +0000

    Add Task 4

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,16 +26,4 @@ def index() -> str:
     Return:
       - 3-index.html
     """
-    return render_template('3-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """ Determines best match for supported languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    return render_template('3
